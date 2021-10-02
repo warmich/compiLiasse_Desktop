14d2b978030285545903bc9af0022877d5409d7b
warmich <75064346+warmich@users.noreply.github.com>
2021-10-02 17:30:56 +0200

    Ajouts

---
+++ compiLiasse_Desktop/Views/F_DataGrid.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace compiLiasse_Desktop.Views
+{
+	/// <summary>
+	/// Logique d'interaction pour F_DataGrid.xaml
+	/// </summary>
+	public partial class F_DataGrid : Window
+	{
+		public F_DataGrid()
+		{
+			InitializeComponent();
+		}
+	}
+}
